c087b9068562d19d9bc9013fa7de8b32463f3148
XabierGM <47028460+XabierGM@users.noreply.github.com>
2021-12-02 10:32:45 +0100

    Subida dos traballos de novembro

---
+++ flexbox/README.md
@@ -0,0 +1,2 @@
+# flexbox
+ Tienda hecha con flexbox. Usado el código que se puede encontrar en css tricks más adaptado a la cuestiòn de la que trataba el ejercicio.
---
+++ formulario/script.js
@@ -0,0 +1,9 @@
+let startingPokerChips = 100; 
+let players = 3;
+let noOfStarterCards = 2;
+
+let playerOnePoints = startingPokerChips;
+let playerTwoPoints = startingPokerChips;
+let playerThreePoints = startingPokerChips;
+
+print ("hello world!")
